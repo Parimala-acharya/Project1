c71c06234586b63efe03e5ec702007ddfdbf6378
ManojCM26 <manojcm2611@gmail.com>
2026-08-30 18:22:15 +0530

    corrected the filename

--- src/test/java/Dropdowns/hiddenDropdown.java
+++ src/test/java/Dropdowns/hiddenDropdown.java
@@ -7,9 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import com.sun.tools.javac.util.List;
-
-public class hideenDropdown {
+public class hiddenDropdown {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
